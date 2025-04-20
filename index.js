b5ab0160453cbb42b37d7109cb9b7b9db8db20cf
Fahrihosting1 <157112223+Fahrihosting1@users.noreply.github.com>
2025-04-20 21:12:26 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+// server.js atau file API utama
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Const untuk data respons
+const apiInfo = {
+    secretCode: "HamzAS",
+    description: "PREMIUM KEY",
+    lastUpdated: "2025-04-04",
+    author: "Hamz",
+    status: "Active",
+    license: "MIT",
+    version: "1.0.0"
+};
+
+// Endpoint untuk langsung membaca data
+app.get("/api/info", (req, res) => {
+    res.json(apiInfo);
+});
+
+// Menyalakan server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
